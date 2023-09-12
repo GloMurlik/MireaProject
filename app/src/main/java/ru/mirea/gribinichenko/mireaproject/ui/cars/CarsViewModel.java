4497866d34364a2cc712fe8661b4d19f1b55f0ef
Glo_Murlik <gribinichenko.danil@yandex.ru>
2023-09-12 14:41:38 +0300

    Initial commit

---
+++ app/src/main/java/ru/mirea/gribinichenko/mireaproject/ui/cars/CarsViewModel.java
@@ -0,0 +1,7 @@
+package ru.mirea.gribinichenko.mireaproject.ui.cars;
+
+import androidx.lifecycle.ViewModel;
+
+public class CarsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/ru/mirea/gribinichenko/mireaproject/ui/legends/MoreInfoViewModel.java
@@ -0,0 +1,7 @@
+package ru.mirea.gribinichenko.mireaproject.ui.legends;
+
+import androidx.lifecycle.ViewModel;
+
+public class MoreInfoViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
